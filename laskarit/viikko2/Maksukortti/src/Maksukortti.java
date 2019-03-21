2e844b3e9ac281937e658e730ae85c65caa99926
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2019-03-21 13:32:07 +0200

    Maksukortti luotu

---
+++ laskarit/viikko2/Maksukortti/src/Maksukortti.java
@@ -0,0 +1,37 @@
+public class Maksukortti {
+    private double arvo;
+    private final double EDULLINEN = 2.5;
+    private final double MAUKAS = 4.0;
+ 
+    public Maksukortti(double arvoaAlussa) {
+        this.arvo = arvoaAlussa;
+    }
+ 
+    public void syoEdullisesti() {
+        if (this.arvo >= EDULLINEN) {
+            this.arvo -= EDULLINEN;
+        }
+    }
+ 
+    public void syoMaukkaasti() {
+        if (this.arvo > MAUKAS) {
+            this.arvo -= MAUKAS;
+        }
+    }
+ 
+    public void lataaRahaa(double rahamaara) {
+        if (rahamaara < 0) {
+            return;
+        }
+ 
+        this.arvo += rahamaara;
+        if (this.arvo > 150) {
+            this.arvo = 150;
+        }
+    }
+ 
+    @Override
+    public String toString() {
+        return "Kortilla on rahaa " + this.arvo + " euroa";
+    }
+} 
