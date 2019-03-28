ac62c543435dcb40d5d60d4b08c50ceac8894055
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2019-03-28 18:10:47 +0200

    create SceneHandler

--- JumpingGame/src/main/java/construction/SceneConstructor.java
+++ JumpingGame/src/main/java/construction/SceneConstructor.java
@@ -5,10 +5,33 @@
  */
 package construction;
 
-/**
- *
- * @author sonja
- */
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
 public class SceneConstructor {
-    
+
+    public Scene startScene() {
+
+        /*
+        BorderPane layout = new BorderPane();
+        VBox buttons = new VBox();
+        
+        Button startGame = new Button("Start Game");
+        Button howToPlay = new Button("How To Play");
+        Button highScores = new Button("High Scores");
+        
+        buttons.getChildren().addAll(startGame, howToPlay, highScores);
+        
+        layout.setTop(new Label("Jumping Game"));
+        layout.setCenter(buttons);              
+        
+        Scene startScene = new Scene(layout);
+         */
+        return startScene;
+
+    }
+
 }
--- JumpingGame/src/main/java/ui/JumpingGameUi.java
+++ JumpingGame/src/main/java/ui/JumpingGameUi.java
@@ -5,37 +5,43 @@
  */
 package ui;
 
+import construction.SceneConstructor;
+import logic.SceneHandler;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.*;
+import javafx.stage.Stage;
 
-public class JumpingGameUi extends Application{
-    
+public class JumpingGameUi extends Application {
+
     @Override
     public void start(Stage stage) {
-        
+
+        /*
         Button startGame = new Button("Start game");
         Button stopGame = new Button("Stop game");
-        
+
         Scene startScene = new Scene(startGame);
         Scene game = new Scene(stopGame);
-        
+
         startGame.setOnAction((event) -> {
             stage.setScene(game);
         });
-        
+
         stopGame.setOnAction((event) -> {
-           stage.setScene(startScene); 
+            stage.setScene(startScene);
         });
+         */
         
-        stage.setScene(startScene);
+        SceneHandler sceneHandler = new SceneHandler(stage);
+
         stage.show();
-        
+
     }
-    
+
     public static void main(String args[]) {
         launch(JumpingGameUi.class);
     }
-    
+
 }
